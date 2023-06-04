297cc84e4437e398b0108269e61d4d6efce7e908
yeonding <yeonding2@naver.com>
2023-06-04 17:04:32 +0900

    [수정|0604] footerJS삭제, POM.xml 수정

--- pearling/src/main/resources/static/js/footer/footer.js
+++
@@ -1,6 +0,0 @@
-fetch("/html/footer/footer.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("footer").innerHTML = data;
-  })
-  .catch((error) => console.error(error));

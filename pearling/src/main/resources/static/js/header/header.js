fb0bc6ac9c6a2b9c655b05556e0ca32ad66ef869
one4961 <one496100@gmail.com>
2023-06-04 15:22:49 +0900

    [추가|0604] aside 추가

--- pearling/src/main/resources/static/js/header/header.js
+++ pearling/src/main/resources/static/js/header/header.js
@@ -1,21 +1,15 @@
-fetch("/html/header/header.html")
-  .then((response) => response.text())
-  .then((data) => {
-      document.getElementById("header").innerHTML = data;
-      
-      var header = document.getElementById('header');
-      var menu = header.querySelector(".menu");
-      var subMenu = document.getElementById('sub-menu');
-      var close = subMenu.querySelector('.sub-close');
-      
-      menu.addEventListener('click', function() {
-        subMenu.style.right = "0";
-        console.log("클릭");
-      });
+window.addEventListener("load", function(e) {
+  var header = document.getElementById('header');
+  var menu = header.querySelector(".menu");
+  var subMenu = document.getElementById('sub-menu');
+  var close = subMenu.querySelector('.sub-close');
+  
+  menu.addEventListener('click', function() {
+    subMenu.style.right = "0";
+    console.log("클릭");
+  });
 
-      close.addEventListener('click', function(){
-        subMenu.style.right = "-100%";
-      });
-  })
-  .catch((error) => console.error(error));
-
+  close.addEventListener('click', function(){
+    subMenu.style.right = "-100%";
+  });
+});

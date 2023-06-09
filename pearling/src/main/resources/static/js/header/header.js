de21bc3fe24b8121b3e5b935aced141ce7e42243
yeonding <yeonding2@naver.com>
2023-06-09 12:18:59 +0900

    [수정|0609] aside 및 header css js 수정

--- pearling/src/main/resources/static/js/header/header.js
+++ pearling/src/main/resources/static/js/header/header.js
@@ -1,15 +1,23 @@
 window.addEventListener("load", function(e) {
   let header = document.getElementById('header');
+  let dmf = document.getElementById('dmf');
   let menu = header.querySelector(".menu");
+  let close = header.querySelector('.sub-close')
   let subMenu = document.getElementById('sub-menu');
-  let close = subMenu.querySelector('.sub-close');
   
   menu.addEventListener('click', function() {
     subMenu.style.right = "0";
+    menu.style.display = "none";
+    close.style.display = "block";
+    dmf.style.display="none";
     console.log("클릭");
   });
-
+  
   close.addEventListener('click', function(){
     subMenu.style.right = "-100%";
+    menu.style.display = "block"
+    close.style.display = "none";
+    dmf.style.display="block";
+
   });
 });

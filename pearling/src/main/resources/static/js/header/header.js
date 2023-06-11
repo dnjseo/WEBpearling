13cff2489e44231cb953e591500f3644a1cfb1a3
yeonding <yeonding2@naver.com>
2023-06-11 22:04:27 +0900

    [수정|0611] aside js 추가 및 css 수정  &  myshell > 삭제

--- pearling/src/main/resources/static/js/header/header.js
+++ pearling/src/main/resources/static/js/header/header.js
@@ -20,4 +20,53 @@ window.addEventListener("load", function(e) {
     dmf.style.display="block";
 
   });
+
+
+  // 오늘 날짜 업데이트
+   
+  let todayIndex = 0;
+  const todaySection = document.querySelector(".paragraph");
+  
+  updateDate(todayIndex); // 현재 날짜로 업데이트
+
+function updateDate(offset) {
+    let today = new Date();
+    today.setDate(today.getDate() + offset);
+    let month = today.getMonth();
+    let day = today.getDate().toString().padStart(2, '0');
+  
+
+    switch (month) 
+    {
+      case 0: month = "01"; break;
+      case 1: month = "02"; break;
+      case 2: month = "03"; break;
+      case 3: month = "04"; break;
+      case 4: month = "05"; break;
+      case 5: month = "06"; break;
+      case 6: month = "07"; break;
+      case 7: month = "08"; break;
+      case 8: month = "09"; break;
+      case 9: month = "10"; break;
+      case 10: month = "11"; break;
+      case 11: month = "12"; break;
+    }
+
+    switch(day)
+    {
+        case 0 : day = "sun"; break; 
+        case 1 : day = "mon"; break;
+        case 2 : day = "tue"; break;
+        case 3 : day = "wed"; break;
+        case 4 : day = "thu"; break;
+        case 5 : day = "fri"; break;
+        case 6 : day = "sat"; break;
+    }
+    
+    todaySection.querySelector('.todayAside').innerText = month+"월 "+day+"일";
+  
+}
+
+
+
 });

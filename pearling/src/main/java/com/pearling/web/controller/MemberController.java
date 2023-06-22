f8f14b33a9fec8d98093178c261bdcc49804cc71
one4961 <one496100@gmail.com>
2023-06-22 11:32:46 +0900

    [추가|0622] 회원 수정시 .. 프로필 이미지 변경 .... 근데 이제 실시간으로 수정되어야 함 ... 그것만 바꾸면 끝

--- pearling/src/main/java/com/pearling/web/controller/MemberController.java
+++ pearling/src/main/java/com/pearling/web/controller/MemberController.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +20,8 @@ import org.springframework.web.multipart.MultipartFile;
 import com.pearling.web.entity.Member;
 import com.pearling.web.security.MyUserDetails;
 import com.pearling.web.service.MemberService;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 
 @Controller
@@ -51,44 +55,115 @@ public class MemberController {
     public String profileSubmit(@ModelAttribute Member member,
                                 @AuthenticationPrincipal MyUserDetails user,
                                 @RequestParam("file") MultipartFile file,
-                                Model model) {
+                                HttpServletRequest request,
+                                Model model) throws IOException {
         Member existingMember = service.getByUsername(user.getUsername());
 
         if (existingMember != null) {
             existingMember.setName(member.getName());
             existingMember.setNickname(member.getNickname());
 
-            try {
-                // 이미지가 존재하는 경우에만 업로드 처리를 수행합니다.
-                String profileImagePath = service.uploadProfileImage(file); // 프로필 이미지 업로드 및 경로 설정
-                System.out.println("너 존재하니 ????/");
-                if (profileImagePath != null) {
-                    existingMember.setProfileImage(profileImagePath);
-                }
-                System.out.println(profileImagePath);
-
-                int result = service.updateMember(existingMember);
-
-                if (result != 0) {
-                    user.setName(member.getName());
-                    user.setNickname(member.getNickname());
-                    user.setProfileImage(member.getProfileImage());
-                }
-
-                // 수정된 member 객체를 다시 모델에 추가합니다.
-                model.addAttribute("member", existingMember);
-
-                return "redirect:/setting/profile";
-            } catch (IOException e) {
-                // 파일 업로드 오류 처리
-                e.printStackTrace();
-                System.out.println("못찾겠다... 하");
-                // 여기서 오류 처리 로직을 추가해주세요.
+            if (!file.isEmpty()) {
+                // 파일 업로드를 MemberService를 통해 처리
+                String fileName = service.uploadProfileImage(file, request);
+                existingMember.setProfileImage(fileName);
+                System.out.println("나 업로드 중이다아앙!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             }
+
+            // 서비스 계층의 업데이트 코드를 제거하였으므로 해당 부분을 수정해야합니다.
+            int result = service.updateMember(existingMember);
+
+            if (result != 0) {
+                user.setName(member.getName());
+                user.setNickname(member.getNickname());
+                user.setProfileImage(member.getProfileImage());
+                System.out.println("이미지!@@@@@" + member.getProfileImage());
+            }
+
+            // 수정된 member 객체를 다시 모델에 추가합니다.
+            model.addAttribute("member", existingMember);
+
+            // 다른 작업이나 리디렉션을 처리하기 위해 코드를 수정합니다.
+
+            return "redirect:/setting/profile";
         }
 
-        return "redirect:/setting/profile"; // 회원이 존재하지 않을 경우 메인 페이지로 리다이렉트
+        return "redirect:/setting/profile";
     }
+
+    // @PostMapping("/profile")
+    // public String profileSubmit(@ModelAttribute Member member,
+    //                             @AuthenticationPrincipal MyUserDetails user,
+    //                             @RequestParam("file") MultipartFile file,
+    //                             Model model) {
+    //     Member existingMember = service.getByUsername(user.getUsername());
+
+    //     if (existingMember != null) {
+    //         existingMember.setName(member.getName());
+    //         existingMember.setNickname(member.getNickname());
+
+    //         if (!file.isEmpty()) {
+    //             // 파일을 데이터베이스에 저장합니다.
+    //             String originalFilename = file.getOriginalFilename();
+    //             // String fileName = UUID.randomUUID().toString() + "." + FilenameUtils.getExtension(originalFilename);
+    //             existingMember.setProfileImage(originalFilename);
+    //         }
+
+    //         // 서비스 계층의 업데이트 코드를 제거하였으므로 해당 부분을 수정해야합니다.
+    //         int result = service.updateMember(existingMember);
+
+    //         if (result != 0) {
+    //             user.setName(member.getName());
+    //             user.setNickname(member.getNickname());
+    //             user.setProfileImage(member.getProfileImage());
+    //         }
+
+    //         // 수정된 member 객체를 다시 모델에 추가합니다.
+    //         model.addAttribute("member", existingMember);
+
+    //         // 다른 작업이나 리디렉션을 처리하기 위해 코드를 수정합니다.
+
+    //         return "redirect:/setting/profile";
+    //     }
+
+    //     return "redirect:/setting/profile";
+    // }
+
+
+    // @PostMapping("/uploadProfileImage")
+    // public ResponseEntity<?> uploadProfileImage(@RequestParam("file") MultipartFile file,
+    //     HttpServletRequest request) throws IOException {
+    //     // 파일 업로드 처리
+    //     if (!file.isEmpty()) {
+    //         String filePath = "resources/img/";
+    //         String realPath = request.getServletContext().getRealPath(filePath);
+
+    //         System.out.println(realPath);
+
+    //         File pathFile = new File(realPath);
+    //         if (!pathFile.exists())
+    //             pathFile.mkdir();
+
+    //         String originalFilename = file.getOriginalFilename();
+    //         // String fileName = UUID.randomUUID().toString() + "." + FilenameUtils.getExtension(originalFilename);
+    //         // System.out.println("이것은 이름입니다!!!!!!!"+fileName);
+    //         String fullPath = realPath + File.separator + originalFilename;
+
+    //         try {
+    //             // 파일 저장
+    //             file.transferTo(new File(fullPath));
+    //             // 파일 저장 성공 시, 업로드된 파일의 경로를 JSON 형태로 응답
+    //             return ResponseEntity.ok().body("{\"filePath\": \"/" + filePath + originalFilename + "\"}");
+    //         } catch (IOException e) {
+    //             // 파일 저장 중 에러 발생 시 예외 처리
+    //             e.printStackTrace();
+    //             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+    //                     .body("파일 업로드 중에 오류가 발생했습니다.");
+    //         }
+    //     } else {
+    //         return ResponseEntity.badRequest().body("업로드할 파일을 선택해주세요.");
+    //     }
+    // }
 
  
 
--- pearling/src/main/resources/static/js/setting/setting-profile.js
+++ pearling/src/main/resources/static/js/setting/setting-profile.js
@@ -1,7 +1,6 @@
 window.addEventListener("load", function () {
     var modify = document.querySelector('.modify'); // 수정 버튼
     var cancelButton = document.querySelector('.cancel'); // 취소 버튼
-    var editButton = document.querySelector('.edit'); // 편집 버튼
 
     var modifyModal = document.getElementById("modify-modal");
     var modifyYes = document.querySelector('.modify-yes');
@@ -13,15 +12,27 @@ window.addEventListener("load", function () {
     });
 
     // 수정하기 버튼 클릭 시
-    // modify.addEventListener("click", function (event) {
-    //     event.preventDefault(); // 폼 제출 막기
-    //     modifyModal.style.display = "block"; // 수정 완료 창 표시
-    // });
+    modify.addEventListener("click", function (event) {
+        event.preventDefault(); // 폼 제출 막기
+        modifyModal.style.display = "block"; // 수정 완료 창 표시
+    });
 
     // 수정 완료 창 확인 버튼 클릭 시
-    // modifyYes.addEventListener("click", function (event) {
-    //     console.log("보내진다 ㅎㅎ");
-    //     modifyModal.style.display = "none";
-    // });
+    modifyYes.addEventListener("click", function (event) {
+        console.log("보내진다 ㅎㅎ");
+        modifyModal.style.display = "none";
+    });
+
+    // 파일 선택 시 이미지 미리보기
+    document.getElementById("fileInput").addEventListener("change", function(event) {
+    var input = event.target;
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        document.querySelector(".profile_img").setAttribute("src", e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  });
 
 });

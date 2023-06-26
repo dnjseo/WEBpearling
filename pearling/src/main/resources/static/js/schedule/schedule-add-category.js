16a86b8ec77e16a2f9f26e7e425f42e92c7fe8b3
yeonding <yeonding2@naver.com>
2023-06-26 12:57:42 +0900

    [추가|0626] 스케쥴 카테고리 컬러 선택 가능

--- pearling/src/main/resources/static/js/schedule/schedule-add-category.js
+++ pearling/src/main/resources/static/js/schedule/schedule-add-category.js
@@ -32,6 +32,12 @@ window.addEventListener("load", function(e) {
         let colorIndex = Array.from(colBall).indexOf(e.target);
         let selectedColor = pallet[colorIndex];
 
+        let input = document.createElement("input");
+        input.type = "hidden";
+        input.name = "backgroundColor";
+        input.value = selectedColor.color;
+        document.querySelector(".send-form").appendChild(input);
+
         console.log("선택한 색상:", selectedColor.color);
         console.log("선택한 카테고리:", selectedColor.category);
     };

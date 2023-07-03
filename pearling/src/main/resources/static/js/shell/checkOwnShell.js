631e6d43dfd5e39da6e50e9699b87778563552f1
yeonding <yeonding2@naver.com>
2023-07-03 21:15:33 +0900

    [추가|0703] 다른 회원 스케쥴 디테일 확인 불가

--- pearling/src/main/resources/static/js/shell/checkOwnShell.js
+++ pearling/src/main/resources/static/js/shell/checkOwnShell.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const userId = (pathArray[1] === "shell" && pathArray[2] === "myshell") ? pathArray[3] || null : null;
   const plusBtn = document.querySelector('.plusbox')
   const checkboxes = document.querySelectorAll('.todo-checkbox');
+  const schedules = document.querySelectorAll('.sche-link');
 
   //로그인한 회원 정보 가져오기
   fetch('/api/member')
@@ -22,18 +23,33 @@ document.addEventListener('DOMContentLoaded', function () {
           checkbox.removeAttribute('disabled');
         });
 
-        // 아닐 경우 : 아워쉘 메뉴 비활성화
+        // 본인 쉘이 아닐 경우 
+        // : 아워쉘 메뉴 비활성화
       } else {
         document.getElementById('s1').style.display = "block";
         document.getElementById('s1').style.display = "flex";
         document.getElementById('shell-menu').style.display = "none";
 
-        //마이쉘 하위메뉴 a링크 값 변경
+        //: 마이쉘 하위메뉴 a링크 값 변경
         document.querySelector('#myshell-menu .monthly').href = "/shell/myshell/" + userId
-        document.querySelector('#myshell-menu .diary').href = "/diary/list/" + userId
+        document.querySelector('#myshell-menu .diary').href = "/diary/list?uid=" + userId
         document.querySelector('#myshell-menu .guestbook').href = "/guestbook/list/" + userId
-        
+       
+        // : 프로필 변경 
         changeProfile(userId);
+
+        schedules.forEach(function (schedule) {
+          let scheduleElements = document.querySelector('.scheduleListSection');
+      
+
+          schedule.href = '#';
+          schedule.onclick = function (event) {
+            event.preventDefault(); // 클릭 이벤트 막기
+            return false;
+          }
+        });
+        
+        
 
 
       }
@@ -43,6 +59,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
 }); //DOM end
 
+
+// 쉘 프로필 변경하는 로직.
 function changeProfile(userId){
   fetch(`/api/member/${userId}`)
   .then(response => response.json())

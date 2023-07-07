fca37f54a6d524583baf1e5a3e346d44cba7b8d9
yrnc <clickq01@gmail.com>
2023-07-07 09:32:13 +0900

    [추가|0707] 친구쉘에서 팔로우/언팔로우 구현

--- pearling/src/main/java/com/pearling/web/api/controller/ShellController.java
+++ pearling/src/main/java/com/pearling/web/api/controller/ShellController.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,71 +31,82 @@ import com.pearling.web.service.TodoService;
 @RequestMapping("api/shell")
 public class ShellController {
 
-    @Autowired
-    FollowService followService;
+  @Autowired
+  FollowService followService;
 
-    @Autowired
-    TodoService todoService;
+  @Autowired
+  TodoService todoService;
 
-    @Autowired
-    ScheduleService scheduleService;
+  @Autowired
+  ScheduleService scheduleService;
 
-    @GetMapping("myshell")
-      public List<Member> myFollowingList(){
+  @GetMapping("myshell")
+    public List<Member> myFollowingList(){
+
+      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      MyUserDetails user = (MyUserDetails) authentication.getPrincipal();
+      int memberId = user.getId();
+      
+      List<Member> friendList = followService.getFollowingsList(memberId);
+
+      return friendList;
+    }
+
+    @GetMapping("myshell/{id}")
+      public List<Member> checkMyFollowList(){
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         MyUserDetails user = (MyUserDetails) authentication.getPrincipal();
         int memberId = user.getId();
         
-        List<Member> friendList = followService.getFollowingsList(memberId);
+        List<Member> followList = followService.getFollowingsList(memberId);
 
-        return friendList;
-      }
-
-    @PostMapping("myshell/{id}")
-      public void addFollow(  @PathVariable("id") int id,
-                              @RequestBody FollowRequest followRequest, 
-                              @AuthenticationPrincipal MyUserDetails user){
-        Follow follow = Follow.builder()
-                        .followerId(followRequest.getFollowerId())
-                        .followingId(user.getId())
-                        .build();
-        followService.addFollow(follow);
+        return followList;
       }
   
-    // @GetMapping("ourshell")
-    //   public List<Member> followingList(){
+    
 
-    //     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    //     MyUserDetails user = (MyUserDetails) authentication.getPrincipal();
-    //     int memberId = user.getId();
-        
-    //     List<Member> friendList = followService.getFollowingsList(memberId);
-
-    //     return friendList;
-    // }
-
-    @GetMapping("ourshell")
-    public List<Object> firendScheduleList(){
-      	SecurityContext context = SecurityContextHolder.getContext();
-	    	MyUserDetails user = (MyUserDetails) context.getAuthentication().getPrincipal();
-
-        int userId = user.getId();
-		    LocalDate todayDate = LocalDate.now();
-
-      	List<Member> friendList = followService.getFollowingsList(userId); 
-	    	List<Todo> friendTodoList = todoService.getListByCurDate(userId,todayDate);	
-        List<Schedule> scheduleList = scheduleService.getListByCurDate(userId, todayDate);
-
-        List<Object> combinedList = new ArrayList<>();
-        
-        combinedList.addAll(friendList);
-        combinedList.addAll(friendTodoList);
-        combinedList.addAll(scheduleList);
-
-
-        return combinedList;
+  @PostMapping("myshell/{id}")
+    public void addFollow(  @PathVariable("id") int id,
+                            @RequestBody FollowRequest followRequest, 
+                            @AuthenticationPrincipal MyUserDetails user){
+      Follow follow = Follow.builder()
+                      .followerId(followRequest.getFollowerId())
+                      .followingId(user.getId())
+                      .build();
+      followService.addFollow(follow);
     }
-  
-}
 
+  
+  @DeleteMapping("myshell/{id}")
+    public void deleteFollow(@PathVariable("id") int id){
+       Follow follow = followService.findById(id);
+        if(follow != null){
+            followService.deleteFollow(follow);
+        }
+    }  
+
+
+  @GetMapping("ourshell")
+  public List<Object> firendScheduleList(){
+      SecurityContext context = SecurityContextHolder.getContext();
+      MyUserDetails user = (MyUserDetails) context.getAuthentication().getPrincipal();
+
+      int userId = user.getId();
+      LocalDate todayDate = LocalDate.now();
+
+      List<Member> friendList = followService.getFollowingsList(userId); 
+      List<Todo> friendTodoList = todoService.getListByCurDate(userId,todayDate);   
+      List<Schedule> scheduleList = scheduleService.getListByCurDate(userId, todayDate);
+
+      List<Object> combinedList = new ArrayList<>();
+      
+      combinedList.addAll(friendList);
+      combinedList.addAll(friendTodoList);
+      combinedList.addAll(scheduleList);
+
+
+      return combinedList;
+  }
+
+}
---
+++ pearling/src/main/resources/static/js/header/friendShell.js
@@ -0,0 +1,107 @@
+window.addEventListener("DOMContentLoaded", function(e) {
+    let realUrl = new URL(window.location.href);
+    let path = realUrl.pathname;
+    let pathArray = path.split("/");
+    let uid = pathArray[pathArray.length - 1];
+    let urlwithId = `http://localhost:8080/api/shell/myshell/${uid}`;
+    let Btn = document.querySelector(".follobutton");
+    
+    // 친구 쉘 header 로드
+    function friendShellLoad(urlwithId) {
+        let friendShellList = document.querySelector(".profile-x");
+        
+        console.log('friendShellLoad 함수 실행됨');
+        console.log('urlwithId:', urlwithId);
+
+        fetch(urlwithId)
+            .then(response => response.json())
+            .then(followList => {
+                friendShellList.innerHTML = "";
+                
+                const idToCheck = uid.toString(); // 검사할 id 값을 현재 페이지의 id로 설정
+                
+                let itemTemplate = `
+                <form class="profile-x">
+                <img class="shell-image" src="/images/profile/profile_yr.png">
+                <p class="shell-name"> 탐정's shell</p>
+                <button class="follobutton">팔로우</button>
+                </form>
+                `;
+
+                const matchingFriend = followList.find(friend => friend.id.toString() === idToCheck)
+                
+                // 팔로잉 상태인 경우 템플릿을 수정하여 출력
+                if (matchingFriend) {
+
+                    // 내 팔로우 리스트에서 현재 페이지 id랑(followerId)랑 같은 값 찾기
+                    let id = matchingFriend.statusId;
+                    console.log(id);
+                    
+                    itemTemplate = `
+                    <form class="profile-x">
+                    <img class="shell-image" src="/images/profile/profile_yr.png">
+                    <p class="shell-name"> 탐정's shell</p>
+                    <button class="follobutton">팔로잉</button>
+                        </form>
+                        `;
+                    }
+                    
+                    friendShellList.insertAdjacentHTML("beforeend", itemTemplate);
+                    
+                    // followBtn 선택 및 이벤트 리스너 등록
+                    let followBtn = document.querySelector(".follobutton");
+                    followBtn.addEventListener('click', function(e){
+                        // e.preventDefault(); // 주석 처리 또는 제거
+                        
+                        let followerId = uid;
+                        console.log("친구의 id는 " + followerId);
+                        let followingIdInput = document.querySelector("input[name='followingHiddenId']");
+                        console.log(followingIdInput);
+                        
+                        let followingId = followingIdInput.value;
+                        console.log("내 id는 " + followingId);
+                        
+                        if(followBtn.innerText == "팔로우"){
+                            fetch(`/api/shell/myshell/${uid}`,{
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify({followingId, followerId})
+                            })
+                            .then(function(response){
+                                if(response.ok){
+                                    followBtn.innerText = '팔로잉';
+                                    followBtn.style.backgroundColor = '#5A2E95';
+                                    console.log("추가 완");
+                                    e.preventDefault(); 
+                                }
+                            })
+                        }
+                        else if (followBtn.innerText == "팔로잉") {
+                            id = matchingFriend.statusId;
+                            fetch(`/api/shell/myshell/${id}`, {
+                                method: 'DELETE', 
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify({ followingId, followerId })
+                            })
+                            .then(function(response) {
+                                if (response.ok) {
+                                    followBtn.innerText = '팔로우';
+                                    followBtn.style.backgroundColor = "";
+                                    console.log("삭제 완" + id);
+                                    e.preventDefault();
+                            
+                                }
+                            });
+                    }
+                });
+            });
+    }
+
+    friendShellLoad(urlwithId);
+});
+
+
